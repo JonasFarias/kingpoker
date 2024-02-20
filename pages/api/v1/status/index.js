7f4a87d36a85ce8ec9cb91b66c4656ce50786bd3
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2024-02-20 18:52:39 +0000

    modificando frase da api

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,7 +1,5 @@
 function status(request, response) {
-  response
-    .status(200)
-    .json({ chave: "Os alunos do curso.dev são pessoas acima da media" });
+  response.status(200).json({ chave: "O melhor grupo de poker da baixada" });
 }
 
 export default status;
