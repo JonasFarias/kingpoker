9a0a8b7924231a4aed049e95b9d64207bd125e4d
Jonas Farias <44181214+JonasFarias@users.noreply.github.com>
2024-02-26 21:38:20 +0000

    deixa o banco de dados mais robusto e finaliza conexões desnecessarias aberta

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,13 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responsyBody = await response.json();
+
+  const parsedUpdateAt = new Date(responsyBody.updated_at).toISOString();
+  expect(responsyBody.updated_at).toEqual(parsedUpdateAt);
+
+  expect(responsyBody.dependencies.database.version).toEqual("16.0");
+  expect(responsyBody.dependencies.database.max_connections).toEqual(100);
+  expect(responsyBody.dependencies.database.opened_connections).toEqual(1);
 });
